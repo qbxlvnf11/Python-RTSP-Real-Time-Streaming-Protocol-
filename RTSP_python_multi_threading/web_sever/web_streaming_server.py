fc739820449a5f9224052dda0556fbfc37e20078
Yong <qbxlvnf11@naver.com>
2023-01-25 08:22:19 +0900

    Update web_streaming_server.py

--- RTSP_python_multi_threading/web_sever/web_streaming_server.py
+++ RTSP_python_multi_threading/web_sever/web_streaming_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, Response
 import cv2
 import webbrowser
+import time
 
 class WebStreamingServer:
 
@@ -19,23 +20,29 @@ class WebStreamingServer:
 		def get_result_frame():
 			
 			while True:
-			
-				if not self.result_queue.empty():
 				
-					# Get frame
-					frame = self.result_queue.get()
-					img = frame.get_img()
-					img_id = frame.get_img_id()
-					#print('img_id:', img_id)
-					
-					# Processing img
-					img = cv2.resize(img, dsize=(resize_width, resize_height), interpolation=cv2.INTER_AREA)
-					img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-		
-					ret, buffer = cv2.imencode('.jpg', img)
-					img = buffer.tobytes()
-					yield (b'--frame\r\n'
-					b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')  # concat frame one by one and show result
+				try:					
+					time.sleep(0.001)
+					if not self.result_queue.empty():
+
+						# Get frame
+						frame = self.result_queue.get()
+						img = frame.get_img()
+						img_id = frame.get_img_id()
+						#print('img_id:', img_id)
+
+						# Processing img
+						img = cv2.resize(img, dsize=(resize_width, resize_height), interpolation=cv2.INTER_AREA)
+						img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+						ret, buffer = cv2.imencode('.jpg', img)
+						img = buffer.tobytes()
+						yield (b'--frame\r\n'
+						b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')  # concat frame one by one and show result
+
+				except Exception as e:
+					print('WebStreamingServer error!', e)
+					print('reopen web!')
 					
 		@self.app.route('/video_feed')
 		def video_feed():
